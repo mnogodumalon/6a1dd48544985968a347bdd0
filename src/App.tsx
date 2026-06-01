180c855c3c5ae7f7ac96ffb8948e95698d83ebe1
Klar <klar@livinglogic.de>
2026-06-01 18:54:01 +0000

    Klar Auto-Deploy

--- src/App.tsx
+++ src/App.tsx
@@ -6,7 +6,6 @@ import { ErrorBoundary } from '@/components/ErrorBoundary';
 import { ErrorBusProvider } from '@/components/ErrorBus';
 import { Layout } from '@/components/Layout';
 import DashboardOverview from '@/pages/DashboardOverview';
-import { WorkflowPlaceholders } from '@/components/WorkflowPlaceholders';
 import AdminPage from '@/pages/AdminPage';
 import TesteingabePage from '@/pages/TesteingabePage';
 import PublicFormTesteingabe from '@/pages/public/PublicForm_Testeingabe';
@@ -26,7 +25,7 @@ export default function App() {
               {/* <public:routes> */}
               {/* </public:routes> */}
               <Route element={<Layout />}>
-                <Route index element={<><div className="mb-8"><WorkflowPlaceholders /></div><DashboardOverview /></>} />
+                <Route index element={<DashboardOverview />} />
                 <Route path="testeingabe" element={<TesteingabePage />} />
                 <Route path="admin" element={<AdminPage />} />
                 {/* <custom:routes> */}
